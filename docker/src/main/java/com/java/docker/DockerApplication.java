58471f2bd1923afe5cbb359f8bec038f5f5be863
徐伟智 <624244232@qq.com>
2019-05-08 16:09:58 +0800

    初始化

---
+++ docker/src/main/java/com/java/docker/DockerApplication.java
@@ -0,0 +1,17 @@
+package com.java.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuweizhi
+ * @since 2019/05/08 10:47
+ */
+@SpringBootApplication
+public class DockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+
+}
