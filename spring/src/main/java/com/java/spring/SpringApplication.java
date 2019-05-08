5fb4a8f8573c603fe10804896276789881a56d0c
徐伟智 <624244232@qq.com>
2019-05-08 16:34:04 +0800

    子项目 Docker Build 整合

--- spring/src/main/java/com/java/spring/SpringApplication.java
+++ spring/src/main/java/com/java/spring/SpringApplication.java
@@ -1,24 +1,24 @@
-package com.java.docker;
+package com.java.spring;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author xuweizhi
- * @since 2019/05/08 10:47
+ * @since 2019/05/08 16:28
  */
 @SpringBootApplication
 @RestController
-public class DockerApplication {
+public class SpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DockerApplication.class, args);
+        org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
     }
 
-    @RequestMapping("/index")
+    @RequestMapping("index")
     public String index() {
-        return "Hello Docker !";
+        return "Hello Spring !";
     }
+
 }
